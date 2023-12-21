bfd357106d9509fb9d5a478cd683edc973edd59c
Joshua Davis <114495200+joshryandavis@users.noreply.github.com>
2023-12-21 13:45:51 +0000

    sync

--- install.sh
+++ install.sh
@@ -20,17 +20,15 @@ if ! chezmoi="$(command -v chezmoi)"; then
 	unset chezmoi_install_script bin_dir
 fi
 
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
-script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-
-set -- init --apply --source="${script_dir}"
-
 echo "Logging into Bitwarden"
 
 bw login || true
 
 export BW_SESSION=$(bw unlock --raw)
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
+set -- init --apply --source="${script_dir}"
 echo "Running 'chezmoi $*'" >&2
 
 exec "$chezmoi" "$@"
